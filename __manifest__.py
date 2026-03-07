6328032d368187224e649b8287d2f46a4d295a5e
oedodynamics-ebihara <hiroyuki.ebihara@oedodynamics.co.jp>
2026-03-07 17:50:41 +0900

    Cleanup unused scaffolding files for Odoo.sh compatibility

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "freee_connect",
+    'name': "freee_conect",
 
     'summary': "Short (1 phrase/line) summary of the module's purpose, used as subtitle on modules listing or apps.openerp.com",
 
@@ -15,7 +15,7 @@ Long description of module's purpose
     # Check https://github.com/odoo/odoo/blob/18.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '19.0.1.0.0',
+    'version': '19.0.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale'],
@@ -24,7 +24,10 @@ Long description of module's purpose
     'data': [
         'views/sale_order_views.xml',
     ],
-    'application': True,
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+    'application': False,
     'installable': True,
     'auto_install': False,
     'license': 'LGPL-3',
